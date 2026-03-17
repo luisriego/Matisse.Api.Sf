4693e2aa9fa85661e316267105b8180d0cbb918e
luisriego <luisriego@hotmail.com>
2026-03-16 22:15:41 -0300

    docs: install and configure NelmioApiDocBundle for OpenAPI spec UI

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
